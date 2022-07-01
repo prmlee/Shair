7f5d3b013a0b0328017eb2ba1a05472a3874e5d6
prmlee <promotionlee.bs@gmail.com>
2022-07-01 04:07:57 -0700

    add: add the datasource file for database configure

---
+++ src/database/data-source.ts
@@ -0,0 +1,17 @@
+import { DataSource } from "typeorm";
+
+const env = process.env.NODE_ENV || 'development';
+
+export const AppDataSource = new DataSource({
+  type: "mysql",
+  host: process.env.MYSQL_HOST,
+  port: Number(process.env.MYSQL_PORT),
+  username: process.env.MYSQL_USERNAME,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+  synchronize: true,
+  logging: false,
+  entities: [env === 'production' ? 'build/database/entities/*{.ts,.js}' : 'src/database/entities/*{.ts,.js}'],
+  migrations: [env === 'production' ? 'build/database/migrations/*{.ts,.js}' : 'src/database/migrations/*{.ts,.js}'],
+  subscribers: [env === 'production' ? 'build/database/subscribers/*{.ts,.js}' : 'src/database/subscribers/*{.ts,.js}'],
+});
