3541cf6770d961d9454bc3be8d115439ad903724
prmlee <promotionlee.bs@gmail.com>
2022-07-02 17:10:45 -0700

    mod: add the CRUD to car service

--- src/services/car.service.ts
+++ src/services/car.service.ts
@@ -1,6 +1,45 @@
+import { CarFilterParam } from "../dtos/filterparam.dto";
 import { AppDataSource } from "../database/data-source";
 import { Car } from "../database/entities/car.entity";
+import { ILike, LessThanOrEqual, MoreThanOrEqual, SelectQueryBuilder } from "typeorm";
 
+const find = async (filterParam: CarFilterParam): Promise<[Car[], number]> => {
+  const query:SelectQueryBuilder<Car> = AppDataSource.getRepository(Car).createQueryBuilder('car');
+  
+  if (filterParam.vin) { 
+    const cars: Car[] = await query.where({ vin: filterParam.vin }).getMany();
+    return [cars, 1];
+  }
+  if (filterParam.licensePlate) { 
+    const cars: Car[] = await query.where({ licensePlate: filterParam.licensePlate }).getMany();
+    return [cars, 1];
+  }
+  
+  if (filterParam.regi) { 
+    query.andWhere({ regi: filterParam.regi });
+  }
+  if (filterParam.regiState) { 
+    query.andWhere({ regiState: filterParam.regiState });
+  }
+  if (filterParam.startDate) {
+    query.andWhere({ regiExp: MoreThanOrEqual(filterParam.startDate) });
+  }
+  if (filterParam.endDate) { 
+    query.andWhere({ regiExp: LessThanOrEqual(filterParam.endDate) });
+  }
+  if (filterParam.startMileage) { 
+    query.andWhere({ currentMileage: MoreThanOrEqual(filterParam.startMileage) });
+  }
+  if (filterParam.endMileage) { 
+    query.andWhere({ currentMileage: LessThanOrEqual(filterParam.endMileage) });
+  }
+  if (filterParam.keyword) { 
+    query.andWhere({ description: ILike(`%${ filterParam.keyword }%`) });
+  }
+
+  const carsAndTotal = query.skip(filterParam.page * filterParam.size).take(filterParam.size).getManyAndCount();
+  return carsAndTotal;
+}
 
 const findById = async (id:string): Promise<Car> => {
   const car: Car = await AppDataSource.getRepository(Car).findOne({
@@ -9,9 +48,13 @@ const findById = async (id:string): Promise<Car> => {
   return car;
 }
 
-const save =async (car: Car): Promise<Car> => {
-  const savedCar: Car = await AppDataSource.getRepository(Car).save(car);
+const save = async (car: Car): Promise<Car> => {
+  const savedCar: Car = await AppDataSource.manager.save(car);
   return savedCar;
 }
 
-export const CarService = { findById, save };
+const remove = async (car: Car): Promise<void> => { 
+  await AppDataSource.getRepository(Car).remove(car);
+}
+
+export const CarService = { find, findById, save, remove };
