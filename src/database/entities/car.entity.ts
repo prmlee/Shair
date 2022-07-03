e3736363ae2ce17eab75af85438371890fb7d715
prmlee <promotionlee.bs@gmail.com>
2022-07-02 17:07:44 -0700

    mod: update the car entity

--- src/database/entities/car.entity.ts
+++ src/database/entities/car.entity.ts
@@ -4,35 +4,35 @@ import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 export class Car
 { 
   @PrimaryGeneratedColumn('uuid')
-  id: string;
+  public id: string;
 
   @Column({ name: 'vin', unique: true, length: 17 })
-  vin: string;
+  public vin: string;
 
   @Column({ name: 'license_plate', unique: true, length: 15 })
-  licensePlate: string;
+  public licensePlate: string;
 
   @Column({ name: 'regi' })
-  regi: string;
+  public regi: string;
 
   @Column({ name: 'regi_state', length: 5 })
-  regiState: string;
+  public regiState: string;
 
   @Column('timestamp', { name: 'regi_expiration' })
-  regiExp: Date;
+  public regiExp: Date;
 
   @Column({ name: 'regi_name', length: 30 })
-  regiName: string;
+  public regiName: string;
 
   @Column({ name: 'car_value', type: 'integer' })
-  carValue: number;
+  public carValue: number;
 
   @Column({ name: 'current_mileage', type: 'integer' })
-  currentMileage: number;
+  public currentMileage: number;
 
   @Column({ name: 'description', type: 'text' })
-  description: string;
+  public description: string;
   
   @Column({ name: 'color', length: 7, default: '#000000' })
-  color: string;
+  public color: string;
 }
