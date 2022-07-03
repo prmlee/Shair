eb81adab83eabdd80f2bff1541b40c1d2e50ccd1
prmlee <promotionlee.bs@gmail.com>
2022-07-02 17:08:37 -0700

    mod: update the car controller - getAll, get, save, delete

--- src/controllers/car.controller.ts
+++ src/controllers/car.controller.ts
@@ -1,40 +1,64 @@
 import { Response } from "express";
-import httpStatus from "http-status";
-import { Body, Controller, Delete, Get, Param, Post, QueryParams, Req, Res } from "routing-controllers";
+import { Body, Delete, Get, JsonController, Param, Post, QueryParams, Res } from "routing-controllers";
 import { CarService } from "../services/car.service";
 import { Car } from "../database/entities/car.entity";
-import { PageQuery } from "../models/PageQuery";
+import { PageQuery } from "../dtos/pagequery.dto";
+import { CarFilterParam } from "../dtos/filterparam.dto";
 
-@Controller('/cars')
+@JsonController('/cars')
 export class CarController
 { 
   @Get('/')
-  async getAll(@QueryParams() query: PageQuery, @Res() response: Response) { 
-    
-    
+  async getAll(@QueryParams() query: PageQuery) { 
+    const filterParam: CarFilterParam = new CarFilterParam();
+    filterParam.page = query.page - 1;
+    filterParam.size = query.size;
+    try {
+      const data = await CarService.find(filterParam);
+      return {data};
+    } catch (error) {
+      console.log(error);
+      return { error: "Internal Server Error" };
+    }
   }
 
   @Get('/:id')
-  async getCar(@Param('id') id: string, @Res() response: Response) { 
+  async getCar(@Param('id') id: string) { 
     try {
       const car: Car = await CarService.findById(id);
-      return response.status(httpStatus.OK).send({ data: car });
+      if (!car) {
+        return { error: "Car Doesn't Exist" };
+      }
+      return {data: car};
     } catch (error) {
-      return response.status(httpStatus.INTERNAL_SERVER_ERROR).json({ error: 'Internal Server Error' });
+      console.log(error);
+      return { error: "Internal Server Error" };
     }
   }
 
   @Post('/')
-  saveCar(@Req() request: Request, @Res() response: Response) { 
+  async saveCar(@Body() car: Car) { 
     try {
-      console.log(request.body);
+      const savedCar: Car = await CarService.save(car);
+      return {data: savedCar};
     } catch (error) {
-      
+      console.log(error);
+      return { error: "Internal Server Error" };
     }
   }
 
   @Delete('/:id')
-  deleteCar(@Param('id') id: string, @Res() response: Response) { 
-    
+  async deleteCar(@Param('id') id: string, @Res() response: Response) { 
+    try {
+      const car: Car = await CarService.findById(id);
+      if (!car) {
+        return { error: "Car Doesn't Exist" };
+      }
+      await CarService.remove(car);
+      return { data: `Removed Car - ${ id }` };
+    } catch (error) {
+      console.log(error);
+      return { error: "Internal Server Error" };
+    }
   }
 }
