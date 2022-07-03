9987a154f38ccb5f095d065f229f1f39346d329c
prmlee <promotionlee.bs@gmail.com>
2022-07-03 11:38:55 -0700

    fix: add the optional class validator to car filter class

--- src/dtos/filterparam.dto.ts
+++ src/dtos/filterparam.dto.ts
@@ -1,37 +1,48 @@
-import { IsAlpha, IsDate, IsNumber, IsPositive } from "class-validator";
+import { IsAlpha, IsDate, IsNumber, IsOptional, IsPositive } from "class-validator";
 import { PageQuery } from "./pagequery.dto";
 
-export class CarFilterParam extends PageQuery { 
+export class CarFilterParam extends PageQuery {
   @IsAlpha()
+  @IsOptional()
   vin: string;
 
   @IsAlpha()
+  @IsOptional()
   licensePlate: string;
 
   @IsAlpha()
+  @IsOptional()
   regi: string;
 
   @IsAlpha()
+  @IsOptional()
   regiState: string;
 
   @IsDate()
+  @IsOptional()
   startDate: Date;
 
   @IsDate()
+  @IsOptional()
   endDate: Date;
 
   @IsPositive()
+  @IsOptional()
   startValue: number;
 
   @IsPositive()
+  @IsOptional()
   endValue: number;
 
   @IsNumber()
+  @IsOptional()
   startMileage: number;
 
   @IsNumber()
+  @IsOptional()
   endMileage: number;
 
   @IsAlpha()
-  keyword: number;
+  @IsOptional()
+  keyword: string;
 }
